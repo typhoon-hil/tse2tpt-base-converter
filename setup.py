f165314de26425ec1dd7cffa966236c25da4d5b7
Marcos Moccelini <marcos.moccelini@typhoon-hil.com>
2022-09-19 16:52:29 -0300

    0.3.0

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tse_to_third_party_tools_converter',
-    version='1.0',
-    packages=find_packages(),
+    name='tse_to_opendss',
+    version='0.3.0',
+    packages=find_packages(exclude=['tests', ]),
     install_requires=["typhoon-hil-api"],
     url='https://www.typhoon-hil.com/',
     include_package_data=True,
     license='MIT',
     author='Marcos Moccelini',
     author_email=f'marcos.moccelini@typhoon-hil.com',
-    description='Typhoon HIL Schematic Editor to third-party tools converter'
+    description='Typhoon HIL Schematic Editor to OpenDSS converter'
 )
